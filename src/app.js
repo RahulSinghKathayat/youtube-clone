36ccfbfd1b97247ced7f346f5fe71865aadc87a1
RahulSinghKathayat <rahulkathayat000@gmail.com>
2025-02-10 00:46:00 +0530

    util files updated

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ import cookieParser from "cookie-parser"
 
 const app = express()
 app.use(cors({
-    origin: CORS_ORIGIN,
+    origin: process.env.CORS_ORIGIN,
     credentials: true
 }))
 
@@ -12,10 +12,15 @@ app.use(express.json({
     limit: "16kb"
 }))
 
+// express.urlencoded will encode the your url 
+// and with the use of extended command the objects in the url will futher divided into objects. this will nested ur url
 app.use(express.urlencoded({
     extended:true, 
     limit:"16kb"
 }))
 
+//express.static will store the files into ur local sever or ur system similar to the "public" file we have in the backend folder
 app.use(express.static("public"))
 app.use(cookieParser())
+
+export { app }
