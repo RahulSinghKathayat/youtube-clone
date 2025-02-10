ccf4e446cb25657cb1dc87273ada2f8d518ffdd7
RahulSinghKathayat <rahulkathayat000@gmail.com>
2025-02-11 00:39:38 +0530

    same controlllers

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -2,6 +2,8 @@ import { asyncHandler } from "../utils/asyncHandler.js"
 import { ApiError } from "../utils/apiError.js"
 import { User } from "../models/user.model.js"
 import { uploadOnCloudinary } from "../utils/cloudinary.js"
+import { ApiResponse } from "../utils/apiResponse.js"
+import { response } from "express"
 
 const registerUser = asyncHandler(async(req, res) => {
     const { fullName, email, username, password } = req.body
@@ -29,15 +31,26 @@ const registerUser = asyncHandler(async(req, res) => {
     if(!avatar){
         throw new ApiError(400, "Avatar file is required")
     }
-    User.create({
+    const user = await User.create({
         fullName,
         avatar: avatar.url,
         coverImage: coverImage?.url || "",
         email,
         password,
         username: username.toLowerCase()
-
     })
+
+    const createdUser = await User.findById(user._id).select(
+        "-password -refreshToken"
+    )
+    
+    if(!createdUser){
+        throw new ApiError(500, "something went wrong while registering the user")
+    }
+
+    return res.status(201).json(
+        new ApiResponse(200, createdUser, "user registered successfully")
+    )
     
 })
 
